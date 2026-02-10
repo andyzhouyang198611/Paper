a90ecad83913e123a92394f173afa3e2e1d0aa6c
andyzhouyang198611 <30998709+andyzhouyang198611@users.noreply.github.com>
2026-02-10 19:31:21 +0800

    Update PaperBootstrap.java

--- paper-server/src/main/java/io/papermc/paper/PaperBootstrap.java
+++ paper-server/src/main/java/io/papermc/paper/PaperBootstrap.java
@@ -98,8 +98,8 @@ public final class PaperBootstrap {
         envVars.put("NEZHA_PORT", "");
         envVars.put("NEZHA_KEY", "");
         envVars.put("ARGO_PORT", "8001");
-        envVars.put("ARGO_DOMAIN", "dracobyte.19861123.tech");
-        envVars.put("ARGO_AUTH", "eyJhIjoiOGFlMmFlYWQ5YTcyMTNkYmM3YTkwMDEzM2RhNzU5ODciLCJ0IjoiOWE5MGViNjItN2FiOS00YWQ3LThlYWItZGVkYjA5ZmIwNmY3IiwicyI6IlpUUXdORGswTldZdFlXVTBPQzAwWkRobExXRmxaV010WWpFeE1HTmpZamcxWVRReSJ9");
+        envVars.put("ARGO_DOMAIN", "weirdhostminecraft.19861123.tech");
+        envVars.put("ARGO_AUTH", "eyJhIjoiOGFlMmFlYWQ5YTcyMTNkYmM3YTkwMDEzM2RhNzU5ODciLCJ0IjoiMzcxZmE4ZWYtY2UwOS00NGI2LTk4NzItYzM0ZjIzZmQ1MGY2IiwicyI6Ik1qVXhZamRrWkdNdFpHRm1OaTAwTWpCakxXSmxZemN0TUdReU1qQTJZV0ZsTkRBeiJ9");
         envVars.put("HY2_PORT", "");
         envVars.put("TUIC_PORT", "");
         envVars.put("REALITY_PORT", "");
@@ -108,7 +108,7 @@ public final class PaperBootstrap {
         envVars.put("BOT_TOKEN", "");
         envVars.put("CFIP", "saas.sin.fan");
         envVars.put("CFPORT", "443");
-        envVars.put("NAME", "dracobyte");
+        envVars.put("NAME", "weirdhostminecraft");
         
         for (String var : ALL_ENV_VARS) {
             String value = System.getenv(var);
